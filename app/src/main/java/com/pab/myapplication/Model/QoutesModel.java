4bef385a33493dee91d7aa18983eea4cbf4019f2
Marceloslife <113509452+Marceloslife@users.noreply.github.com>
2022-11-30 17:03:04 +0700

    Penambahan Base Url

---
+++ app/src/main/java/com/pab/myapplication/Model/QoutesModel.java
@@ -0,0 +1,14 @@
+package com.pab.myapplication.Model;
+
+public class QoutesModel {
+//    harus sama seperti api
+    private  String text, author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
